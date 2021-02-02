b24b7d8e8c0e56307c483c01798d73485de22b18
mrendzzx <mr.endzzx@gmail.com>
2021-02-02 16:13:50 +0700

    fix export listsiswa excel

--- application/controllers/Kartu.php
+++ application/controllers/Kartu.php
@@ -808,11 +808,13 @@ class Kartu extends Core_Controller{
 
 	        $writer = new PHPExcel_Writer_Excel2007($objPHPExcel);
 	        $writer->save(APPPATH.'../'. $filepath .$filename);
+	        sleep(1);
 
 			header('Content-Type: application/octet-stream');
 			header("Content-Transfer-Encoding: Binary");
 			header("Content-disposition: attachment; filename=".$filename);
 			readfile($fileurl); 
+			sleep(5);
 
 	        unlink(APPPATH.'../'. $filepath .$filename);
     	}
